19d8f3ee6f657c3a3f44a3aaf9aff1c546fc41e8
FatemaAkter11 <fatemaakter4261@gmail.com>
2022-02-07 19:28:20 +0600

    banner done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Carousel } from "react-bootstrap";
+import slide1 from "../../../images/img-1.jpg";
+import slide2 from "../../../images/img-2.jpg";
+import slide3 from "../../../images/img-3.jpg";
 
 const Banner = () => {
     return (
-        <div>
-            <h2>This is banner</h2>
-        </div>
+        <Carousel>
+            <Carousel.Item>
+                <img className="d-block w-100" src={slide1} alt="First slide" />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="d-block w-100" src={slide2} alt="Second slide" />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="d-block w-100" src={slide3} alt="Third slide" />
+            </Carousel.Item>
+        </Carousel>
     );
 };
 
